5966583e853683d159787bf13844ff34f960701c
12r1ks <rirsank@gmail.com>
2026-03-29 15:35:51 +0200

    READY_FOR_DEPL

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-import os
-from app import create_app, db
+from app import create_app
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run()
